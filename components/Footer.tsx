21ab42fd14edba40d94ead3949dccc06f119baae
Nihar Hegde <niharhegde163@gmail.com>
2023-09-22 22:27:51 +0530

    basic project setup done

---
+++ components/Footer.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+   <footer className='bg-white border-t'>
+    <div className='mx-auto py-10'>
+        <p className='text-center text-xs text-black'>
+            &copy; 2023 Nihar Hegde | All rights reserved | Code with Antonio.
+        </p>
+    </div>
+   </footer>
+  )
+}
+
+export default Footer

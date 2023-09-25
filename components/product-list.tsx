752bf057ad2c21c678822c830a3ed8b8fbc7e480
Nihar Hegde <niharhegde163@gmail.com>
2023-09-25 20:12:50 +0530

    adding expand and shopping cart icon on prduct card

--- components/product-list.tsx
+++ components/product-list.tsx
@@ -1,6 +1,7 @@
 import { Product } from '@/types';
 import React, { FC } from 'react'
 import NoResults from '@/components/ui/no-results';
+import ProductCard from '@/components/ui/product-card';
 
 interface ProductListProps {
     title:string;
@@ -16,7 +17,7 @@ const ProductList:FC<ProductListProps>= ({title,items}) => {
         <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4'>
           {
             items.map((item)=>(
-              <div key={item.id}>{item.name}</div>
+              <ProductCard key={item.id} data={item}/>
             ))
           }
         </div>

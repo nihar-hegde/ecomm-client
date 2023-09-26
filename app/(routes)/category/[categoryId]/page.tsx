5c647721f1f96e3eb98bdffe5c86bfaa628ac481
Nihar Hegde <niharhegde163@gmail.com>
2023-09-26 21:55:22 +0530

    adding descktop filters

---
+++ app/(routes)/category/[categoryId]/page.tsx
@@ -0,0 +1,54 @@
+import getCategory from "@/actions/get-category";
+import getColors from "@/actions/get-colors";
+import getProducts from "@/actions/get-products";
+import getSizes from "@/actions/get-sizes";
+import Billboard from "@/components/billboard";
+import Container from "@/components/ui/container";
+import React, { FC } from "react";
+import Filter from "./components/filter";
+export const revalidate = 0;
+
+interface CategoryPageProps {
+  params: {
+    categoryId: string;
+  };
+  searchParams: {
+    colorId: string;
+    sizeId: string;
+  };
+}
+
+const CategoryPage: FC<CategoryPageProps> = async ({
+  params,
+  searchParams,
+}) => {
+  const products = await getProducts({
+    categoryId: params.categoryId,
+    colorId: searchParams.colorId,
+    sizeId: searchParams.sizeId,
+  });
+  const sizes = await getSizes();
+  const colors = await getColors();
+  const category = await getCategory(params.categoryId);
+  return (
+    <div className="bg-white">
+      <Container>
+        <Billboard data={category.billboard}></Billboard>
+      </Container>
+      <div className="px-4 sm:px-6 lg:px-8 ph-24">
+        <div className="lg:grid lg:grid-cols-5 lg:pag-x-8">
+            {/* add mobile filters */}
+            <div className="hidden lg:block">
+                <Filter 
+                valueKey="sizeId"
+                name="Size"
+                data={sizes}        
+                />
+            </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CategoryPage;

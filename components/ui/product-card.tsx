2639f39eda2705f2bf1eddfde29a03c90db005ca
Nihar Hegde <niharhegde163@gmail.com>
2023-09-27 22:00:40 +0530

    added preview gallery modal:

--- components/ui/product-card.tsx
+++ components/ui/product-card.tsx
@@ -1,20 +1,27 @@
 "use client";
 import { Product } from "@/types";
 import Image from "next/image";
-import React, { FC } from "react";
+import React, { FC, MouseEventHandler } from "react";
 import IconButton from "./icon-button";
 import { Expand, ShoppingCart } from "lucide-react";
 import Currency from "./currency";
 import { useRouter } from "next/navigation";
+import usePreviewModal from "@/hooks/use-preview-modal";
+
 
 interface ProductCard {
   data: Product;
 }
 
 const ProductCard: FC<ProductCard> = ({ data }) => {
+  const previewModal = usePreviewModal();
   const router = useRouter();
   const handleClick = () => {
     router.push(`/product/${data?.id}`)
+  }
+  const onPreview :MouseEventHandler<HTMLButtonElement> = (event) =>{
+    event.stopPropagation();
+    previewModal.onOpen(data);
   }
   return (
     <div onClick={handleClick} className="bg-white group cursor-pointer rounded-xl border p-3 space-y-4">
@@ -29,7 +36,7 @@ const ProductCard: FC<ProductCard> = ({ data }) => {
         <div className="opacity-0 group-hover:opacity-100 transition absolute w-full px-6 bottom-5">
           <div className="flex gap-x-6 justify-center">
             <IconButton
-              onClick={() => {}}
+              onClick={onPreview}
               icon={<Expand size={20} className="text-gray-600" />}
             />
             <IconButton

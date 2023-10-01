6ca7f6e8fb75a807a8a143efc0591c5679a70a73
Nihar Hegde <niharhegde163@gmail.com>
2023-10-01 21:33:20 +0530

    adding the order summary component

--- app/(routes)/cart/page.tsx
+++ app/(routes)/cart/page.tsx
@@ -3,6 +3,7 @@ import Container from "@/components/ui/container";
 import useCart from "@/hooks/use-cart";
 import React, { useEffect, useState } from "react";
 import CartItem from "./components/cart-item";
+import Summary from "./components/summary";
 
 const CartPage = () => {
   const cart = useCart();
@@ -23,6 +24,7 @@ const CartPage = () => {
                   ))}
                 </ul>
               </div>
+              <Summary />
             </div>
           </div>
         </Container>
